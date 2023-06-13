b1169f64ecc43b0716e12b8d41ac31fd12a13e33
Aamir-20 <aamiromar2002@gmail.com>
2023-06-13 16:18:27 +0100

    something new

--- an-example.py
+++ an-example.py
@@ -5,17 +5,52 @@ Created on Tue Jun 13 11:53:55 2023
 @author: Aamir
 """
 
-from scipy.fft import fft, fftfreq, ifft
+from scipy.fft import fft, fftfreq, ifft, ifftn
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
+
 def main():
-    tut2()
+    tut3()
+    #
+
+def tut3():
+    N = 30
+    f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, sharex='col', sharey='row')
+    xf = np.zeros((N,N))
+    xf[0, 5] = 1
+    xf[0, N-5] = 1
+    Z = ifftn(xf)
+    ax1.imshow(xf, cmap=cm.Reds)
+    ax4.imshow(np.real(Z), cmap=cm.gray)
+    xf = np.zeros((N, N))
+    xf[5, 0] = 1
+    xf[N-5, 0] = 1
+    Z = ifftn(xf)
+    ax2.imshow(xf, cmap=cm.Reds)
+    ax5.imshow(np.real(Z), cmap=cm.gray)
+    xf = np.zeros((N, N))
+    xf[5, 10] = 1
+    xf[N-5, N-10] = 1
+    Z = ifftn(xf)
+    ax3.imshow(xf, cmap=cm.Reds)
+    ax6.imshow(np.real(Z), cmap=cm.gray)
+    plt.show()
+    
 
 def tut2():
-    ...
-##
-
-
+    # Number of sample points
+    N = 600
+    # sample spacing
+    T = 1.0 / 800.0
+    x = np.linspace(0.0, N*T, N, endpoint=False)
+    y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+    yf = fft(y)
+    xf = fftfreq(N, T)[:N//2]
+    plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+    plt.grid()
+    plt.show()
 
 
 def tut1():
