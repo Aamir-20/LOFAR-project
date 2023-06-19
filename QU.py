ea932cc58fa4a1b2b43a7f1591af9ba5a68eb29b
Aamir-20 <aamiromar2002@gmail.com>
2023-06-19 12:47:10 +0100

    QU simulated

--- QU.py
+++ QU.py
@@ -24,19 +24,54 @@ def main():
     # make data regularly spaced in frequency:
     nu = np.linspace(nu_min, nu_max, 512)
     lambda2 = (const_c/nu)**2
-    lambda2 = lambda2[::-1] # reversing list so it's in ascending order.
+
     
-    print("This is lambda squared: ", lambda2)
+    # make data regularly space in lambda^2:
+    #t1 = np.linspace(lambda2_min, lambda2_max, 512)
+         
+    #print("This is lambda squared: ", lambda2)
 
-    Q, U = simulate_QU(0.1, 0.8, 0.9, lambda2)
-    print("This is Q simulated: ", Q)
-    print("This is U simulated: ", U)
+    Q, U = simulate_QU(50, 1.5, 1, lambda2)
+    #print("This is Q simulated: ", Q)
+    #print("This is U simulated: ", U)
+    print("The maximum of Q is: ", max(Q))
+    print("The minimum of Q is: ", min(Q))
+    print("The maximum of U is: ", max(U))
+    print("The minimum of U is: ", min(U))
+    print("The maximum of nu is: ", max(nu))
+    print("The minimum of nu is: ", min(nu))    
+    plot_sim(Q, U, nu, "Frequency")
+    
+    
+    Q, U = simulate_QU(50, 1.5, 1, lambda2)
+    #print("This is Q simulated: ", Q)
+    #print("This is U simulated: ", U)
+    print("The maximum of Q is: ", max(Q))
+    print("The minimum of Q is: ", min(Q))
+    print("The maximum of U is: ", max(U))
+    print("The minimum of U is: ", min(U))
+    print("The maximum of nu is: ", max(nu))
+    print("The minimum of nu is: ", min(nu))    
+    plot_sim(Q, U, lambda2, "$\lambda^2$")    
 
 
-def simulate_QU(phi_0, chi_0, P_0,l2):
+def simulate_QU(phi_0, chi_0, P_0, l2):
     Q = P_0*np.cos(2*(phi_0*l2+chi_0))
     U = P_0*np.sin(2*(phi_0*l2+chi_0))
-    return Q, U    
+    return Q, U
+
+
+def plot_sim(Q, U, nu, title="$\lambda^2$"):
+    ax2 = pl.subplot(111)
+    
+    ax2.plot(nu, Q, linestyle='-', color='c', lw=1.0, label="Q")
+    ax2.plot(nu, U, linestyle='-', color='b', lw=1.0, label="U")
+    ax2.set_ylabel("Q,U")
+    ax2.set_xlabel(title)
+    
+    ax2.legend()
+    pl.show()
+    
     
 if __name__ == "__main__":
     main()
